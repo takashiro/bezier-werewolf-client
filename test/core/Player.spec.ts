0268a0ff0d2c193e52e641f2ea2499abc1267040
Kazuichi Takashiro <takashiro@qq.com>
2022-10-16 16:29:12 +0800

    Add Player

---
+++ test/core/Player.spec.ts
@@ -0,0 +1,8 @@
+import { expect, it } from '@jest/globals';
+
+import Player from '../../src/core/Player';
+
+it('has a seat', () => {
+	const player = new Player(1);
+	expect(player.getSeat()).toBe(1);
+});
